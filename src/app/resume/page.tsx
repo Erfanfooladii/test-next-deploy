b9a902f6d397ac965ed3e45d31be5826c4545a59
Erfanfooladii <arfannnfooladi@gmail.com>
2025-08-14 09:53:00 +0330

    feat: create list

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,6 +2,10 @@ const Page = async () => {
   return (
     <div className="h-screen w-full px-40">
       <h1 className="text-center text-2xl font-bold">Resume page</h1>
+      <ul>
+        <head>list of projects</head>
+        <li></li>
+      </ul>
     </div>
   );
 };
